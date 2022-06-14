bd97af53f9fda0bad733b345f30549dc2d7b3385
vzze <lolo32778@gmail.com>
2022-06-14 11:29:28 +0300

    terminal update

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,13 @@
-Import-Module Terminal-Icons
-
 oh-my-posh init pwsh --config D:\CB\Configs\powershell\ys.omp.json | Invoke-Expression
 
 Set-Alias winfetch pwshfetch-test-1
 
 Set-Alias time Get-Date
+
+function _todo {
+    Get-Content -Path "D:/CB/todo.md"
+}
+
+$TODO = "D:/CB/todo.md"
+
+Set-Alias -Name todo -Value _todo
