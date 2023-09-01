fb67f95d7b832611e3dd30e7725f9b1b9acc637b
vzze <lolo32778@gmail.com>
2023-09-01 03:46:41 +0300

    forgot uncom

--- nvimrc/lua/plugins/util/tidy.lua
+++ nvimrc/lua/plugins/util/tidy.lua
@@ -1,7 +1,7 @@
 return {
     'mcauley-penney/tidy.nvim',
-    -- event = "BufWritePre",
-    -- lazy = true,
+    event = "BufWritePre",
+    lazy = true,
 
     config = function ()
         require("tidy").setup()
