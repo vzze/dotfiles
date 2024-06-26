5efcf9d8c7ce11d1925217352e442d1858d1999b
vzze <lolo32778@gmail.com>
2024-06-26 13:28:00 +0300

    show date and automate screenshots

--- arch/.config/polybar/tidal.sh
+++ arch/.config/polybar/tidal.sh
@@ -6,14 +6,16 @@ function httpGet() {
 
 JSON=$(httpGet current)
 
-TITLE=$(echo "$JSON" | jq -r '.title')
-ARTISTS=$(echo "$JSON" | jq -r '.artist')
-STATUS=$(echo "$JSON" | jq -r '.status')
+TITLE=$(echo "$JSON" | jq -r ".title")
+ARTISTS=$(echo "$JSON" | jq -r ".artist")
+
+STATUS=$(echo "$JSON" | jq -r ".status")
+DATE=$(date +"%F")
 
 INFO=$(echo "$ARTISTS - $TITLE")
 
 if [[ "$STATUS" == "paused" || ${#INFO} -le 3 ]]; then
-    echo ""
+    echo $DATE
 else
-    echo "$INFO"
+    echo "$INFO | $DATE"
 fi
---
+++ arch/.config/scripts/screenshot.sh
@@ -0,0 +1,2 @@
+ksnip -r -p ~/Pictures/screenshot.png
+xclip -i ~/Pictures/screenshot.png -selection clipboard -target image/png
