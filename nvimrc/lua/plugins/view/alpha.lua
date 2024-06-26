59565bd7f9e4c8806d85661e0f8cf3691e2b5729
vzze <lolo32778@gmail.com>
2024-06-26 09:06:00 +0300

    remove priority

--- nvimrc/lua/plugins/view/alpha.lua
+++ nvimrc/lua/plugins/view/alpha.lua
@@ -1,6 +1,5 @@
 return {
     'goolord/alpha-nvim',
-    priority = 40,
     config = function()
         local art = {
             [[                                               aN$8Nx                                       ]],
