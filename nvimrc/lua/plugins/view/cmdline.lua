cb0a1b2396a53a1cce2bca1b17825ee89627bf6f
vzze <lolo32778@gmail.com>
2024-07-31 14:58:31 +0300

    add hl opts to cmdline

--- nvimrc/lua/plugins/view/cmdline.lua
+++ nvimrc/lua/plugins/view/cmdline.lua
@@ -1,6 +1,13 @@
 return {
     "vzze/cmdline.nvim",
     config = function()
-        require("cmdline")()
+        require("cmdline")({
+            hl = {
+                default   = "Pmenu",
+                selection = "Keyword",
+                directory = "Directory",
+                substr    = "Normal"
+            }
+        })
     end
 }
