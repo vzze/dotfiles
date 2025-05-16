07bfe9bc2a91a7ead8712b772c1f806672cfa0d2
vzze <lolo32778@gmail.com>
2025-05-16 01:26:52 +0100

    update to new mason cfg

--- nvimrc/lua/plugins/lsp/lspcfg.lua
+++ nvimrc/lua/plugins/lsp/lspcfg.lua
@@ -2,37 +2,16 @@ return {
     "neovim/nvim-lspconfig",
 
     config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        local lspconfig = require("lspconfig")
+        local capabilities = require("cmp_nvim_lsp")
+            .default_capabilities()
 
-        local function deep_copy(cfg, obj)
-            for key, value in pairs(obj) do
-                if type(value) == "table" and type(cfg[key]) == "table" then
-                    deep_copy(cfg[key], value)
-                else
-                    cfg[key] = value
-                end
-            end
+        local servers = require("mason-lspconfig")
+            .get_installed_servers()
+
+        local wk = require("which-key")
+
+        for _, server in ipairs(servers) do
+            V.lsp.setup_server(server, capabilities, wk)
         end
-
-        require("mason-lspconfig").setup_handlers({
-            function(server)
-                local cfg = {
-                    on_attach = function(client, buf)
-                        require("which-key").add(V.mappings.lsp(buf))
-
-                        -- client.server_capabilities.semanticTokensProvider = nil
-                    end,
-
-                    capabilities = capabilities
-                }
-
-                if V.lsp.custom_configs[server] then
-                    deep_copy(cfg, V.lsp.custom_configs[server])
-                end
-
-                lspconfig[server].setup(cfg)
-            end
-        })
     end
 }
--- nvimrc/lua/v/lsp.lua
+++ nvimrc/lua/v/lsp.lua
@@ -1,10 +1,43 @@
 V.lsp = {}
 
-V.lsp.custom_configs = {}
+V.lsp.capabilities = nil
+
+V.lsp.apply_custom_config = function(cfg, obj)
+    for key, value in pairs(obj) do
+        if type(value) == "table" and type(cfg[key]) == "table" then
+            V.lsp.apply_custom_config(cfg[key], value)
+        else
+            cfg[key] = value
+        end
+    end
+end
+
+V.lsp.setup_server = function(server, capabilities, bind_manager)
+    local cfg = {
+        on_attach = function(client, buf)
+            bind_manager.add(V.mappings.lsp(buf))
+
+            -- client.server_capabilities.semanticTokensProvider = nil
+        end,
+
+        capabilities = capabilities
+    }
+
+    if V.lsp.custom_configs[server] then
+        V.lsp.apply_custom_config(
+            cfg,
+            V.lsp.custom_configs[server]
+        )
+    end
+
+    vim.lsp.config(server, cfg)
+end
 
 V.lsp.diagnostics = {
     virtual_lines = true
 }
+
+V.lsp.custom_configs = {}
 
 V.lsp.custom_configs["lua_ls"] = {
     settings = {
